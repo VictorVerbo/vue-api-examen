b8dcc6406a781285078432bbe951bd9cd93417cc
Victor Manuel Fajardo Hernández <70539048+VictorVerbo@users.noreply.github.com>
2024-03-03 11:30:58 -0600

    Limpieza, depuración y comentado de código

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <div class="title-app">
     <h2 class="title">POKÉDEX INTERNACIONAL</h2>
   </div>
+  <!--Barra de busqueda-->
   <div class="search-container">
     <input type="text" v-model="searchQuery" placeholder="Buscar Pokémon..." class="search-input"
       @keyup.enter="searchPokemon">
     <button @click="searchPokemon" class="btn btn-primary">Buscar</button>
   </div>
+  <!--Lista de pokemons-->
   <div class="pokemon-list">
+    <!--Paginación superior-->
     <div class="pagination">
       <button @click="firstPage" :disabled="offset === 0 || loading" class="btn btn-secondary">Inicio</button>
       <button @click="previousPage" :disabled="offset === 0 || loading" class="btn btn-secondary">Anterior</button>
@@ -16,15 +19,18 @@
       <button @click="lastPage" :disabled="offset + limit >= totalCount || loading"
         class="btn btn-secondary">Final</button>
     </div>
+    <!--Animación de pokeball carga-->
     <div v-if="loading" class="pokeball">
       <div class="pokeball_detalle"></div>
     </div>
+    <!--Carta de pokemon-->
     <div v-else class="pokemon-container">
       <div v-for="pokemon in visiblePokemon" :key="pokemon.id" class="pokemon-card" @click="toggleDetails(pokemon)">
         <img :src="pokemon.imageUrl" :alt="pokemon.name" class="pokemon-image" @error="handleImageError(pokemon)" />
         <div class="pokemon-info">
           <h6 class="pokemon-id"># {{ pokemon.id }}</h6>
           <h5 class="pokemon-name">{{ pokemon.name }}</h5>
+          <!--Detalles de pokemon al dar click-->
           <div v-if="pokemon.showDetails" class="pokemon-details">
             <div>Tipo(s): {{ pokemon.types.join(', ') }}</div>
             <div>Habilidades: {{ pokemon.abilities.join(', ') }}</div>
@@ -32,6 +38,7 @@
         </div>
       </div>
     </div>
+    <!--Paginación inferior-->
     <div class="pagination">
       <button @click="firstPage" :disabled="offset === 0 || loading" class="btn btn-secondary">Inicio</button>
       <button @click="previousPage" :disabled="offset === 0 || loading" class="btn btn-secondary">Anterior</button>
@@ -44,8 +51,7 @@
 </template>
 
 <script>
-import axios from 'axios';
-import { ref } from 'vue';
+import axios from 'axios'
 
 export default {
   data() {
@@ -63,6 +69,7 @@ export default {
     this.fetchPokemonData();
   },
   methods: {
+    //obtención de pokemons consumiendo la api
     fetchPokemonData() {
       this.loading = true;
       axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${this.offset}`)
@@ -76,10 +83,12 @@ export default {
           this.loading = false;
         })
         .catch(error => {
+          alert('Error al obtener información de los Pokémon:', error);
           console.error('Error al obtener información de los Pokémon:', error);
           this.loading = false;
         });
     },
+    //Obetención de detalles del pokemon
     fetchPokemonDetails(url) {
       return axios.get(url)
         .then(response => {
@@ -99,6 +108,7 @@ export default {
           };
         });
     },
+    //Paginación {
     firstPage() {
       this.offset = 0;
       this.fetchPokemonData();
@@ -122,9 +132,12 @@ export default {
         this.fetchPokemonData();
       }
     },
+    //Paginación }
+    //Mostrar los detalles del pokemon
     toggleDetails(pokemon) {
       pokemon.showDetails = !pokemon.showDetails;
     },
+    //Busqueda del pokemon por id o nombre
     searchPokemon() {
       if (this.searchQuery.trim() === '') {
         this.fetchPokemonData();
@@ -134,10 +147,16 @@ export default {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
           .then(response => {
             const pokemon = response.data;
+            let imageUrl = pokemon.sprites.front_default;
+            if (!imageUrl) {
+              // Si no hay una imagen disponible, utilizar una imagen de error predeterminada
+              alert("¡Aun no existe una imagen para este Pokémon!")
+              imageUrl = require("./assets/error.png")
+            }
             const pokemonDetails = {
               id: pokemon.id,
               name: pokemon.name,
-              imageUrl: pokemon.sprites.front_default,
+              imageUrl: imageUrl,
               types: pokemon.types.map(type => type.type.name),
               abilities: pokemon.abilities.map(ability => ability.ability.name),
               showDetails: false
@@ -146,6 +165,7 @@ export default {
             this.loading = false;
           })
           .catch(error => {
+            alert('Error al obtener información del Pokémon:', error);
             console.error('Error al obtener información del Pokémon:', error);
             this.loading = false;
           });
@@ -168,9 +188,8 @@ export default {
   flex-wrap: wrap;
   justify-content: center;
   gap: 20px;
-  /* Espacio entre las tarjetas */
 }
-
+/* Estilo tarjetas*/
 .pokemon-card {
   display: flex;
   flex-direction: column;
@@ -180,7 +199,7 @@ export default {
   border-radius: 5px;
   padding: 10px;
 }
-
+/* Estilo imágenes de tarjetas*/
 .pokemon-image {
   border: 2px solid #000000;
   border-radius: 5px;
@@ -189,33 +208,33 @@ export default {
   margin-bottom: 10px;
   background: linear-gradient(#608d91, #beeef8, #abe1ee, #6fb7c0);
 }
-
+/* Estilo información de tarjetas*/
 .pokemon-info {
   text-align: center;
   background-color: #1e3336;
   color: #b5c1c3;
   border-radius: 5px;
 }
-
+/* Estilo paginación*/
 .pagination {
   margin-top: 20px;
   margin-bottom: 20px;
 }
 
-/* Estilos para los detalles del Pokémon */
+/* Estilo para los detalles del Pokémon */
 .pokemon-details {
   margin-top: 10px;
   margin-left: 5px;
   margin-right: 5px;
   margin-bottom: 5px;
 }
-
+/* Estilo para los nombre Pokémon */
 .pokemon-name {
   text-transform: uppercase;
   margin-left: 5px;
   margin-right: 5px;
 }
-
+/*Estilo del titulo{*/
 .title {
   margin-top: 30px;
   color: #ffffff;
@@ -228,12 +247,14 @@ export default {
   padding: 5px;
   text-align: center;
 }
+/*}Estilo del titulo*/
 
+/*Espaciado de los botones de paginación */
 .btn {
   margin-left: 10px;
   margin-right: 10px;
 }
-
+/* Creación de pokeball y animación*/
 .pokeball {
   width: 10rem;
   height: 10rem;
@@ -293,7 +314,9 @@ export default {
     transform: rotate(360deg);
   }
 }
+/* Creación de pokeball y animación*/
 
+/*Estilo del buscador{ */
 .search-container {
   background-color: #c01c1d;
   display: flex;
@@ -322,6 +345,6 @@ export default {
 
 .search-input::placeholder {
   color: white;
-  /* Cambiar el color del placeholder a blanco */
 }
+/*Estilo del buscador} */
 </style>
